b459634eb76315fb3aca140396ce63386f7cd4e0
fluturecode <fluturecode@gmail.com>
2020-08-18 21:58:24 -0400

    final commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,23 +5,23 @@ import { Avatar } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 
 function Header({ spotify }) {
-  const [{ user }] = useStateValue();
+	const [{ user }, dispatch] = useStateValue();
 
-  return (
-    <div className="header">
-      <div className="header__left">
-        <SearchIcon />
-        <input
-          placeholder="Search for Artists, Songs, or Podcasts "
-          type="text"
-        />
-      </div>
-      <div className="header__right">
-        <Avatar alt={user?.display_name} src={user?.images[0].url} />
-        <h4>{user?.display_name}</h4>
-      </div>
-    </div>
-  );
+	return (
+		<div className="header">
+			<div className="header__left">
+				<SearchIcon />
+				<input
+					placeholder="Search for Artists, Songs, or Podcasts "
+					type="text"
+				/>
+			</div>
+			<div className="header__right">
+				<Avatar src={user?.images[0].url} alt={user?.display_name} />
+				<h4>{user?.display_name}</h4>
+			</div>
+		</div>
+	);
 }
 
 export default Header;
--- src/Components/SongRow.js
+++ src/Components/SongRow.js
@@ -1,7 +1,19 @@
 import React from "react";
+import "./SongRow.css";
 
-function SongRow() {
-  return <div>I am a song row</div>;
+function SongRow({ track, playSong }) {
+	return (
+		<div className="songRow">
+			<img src="" alt="" />
+			<div className="songRow__info">
+				<h1>{track.name}</h1>
+				<p>
+					{track.artists.map((artist) => artist.name).join(", ")}
+					{track.album.name}
+				</p>
+			</div>
+		</div>
+	);
 }
 
 export default SongRow;
